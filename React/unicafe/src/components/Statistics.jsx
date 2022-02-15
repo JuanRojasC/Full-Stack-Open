388543607dbd488afb9f22d880f5265126b5a778
JuanRojasC <appsjuanrojas@gmail.com>
2022-02-15 11:47:44 -0500

    Excercise unicafe step5 1.10

---
+++ React/unicafe/src/components/Statistics.jsx
@@ -0,0 +1,27 @@
+import StatisticLine from "./StatisticLine";
+
+const Statistics = ({good, neutral, bad}) => {
+
+    const totalVotes = good + neutral + bad
+
+    return(
+        <>
+            <h2>statistics</h2>
+            {
+                totalVotes?
+                    <div>
+                        <StatisticLine text='good' value={good} />
+                        <StatisticLine text='neutral' value={neutral} />
+                        <StatisticLine text='bad' value={bad} />
+                        <StatisticLine text='all' value={totalVotes} />
+                        <StatisticLine text='average' value={((good * 1) + (bad * -1)) / totalVotes} />
+                        <StatisticLine text='positive' value={((good / totalVotes) * 100) + ' %'} />
+                    </div>
+                :
+                    <div>No feedback given</div>
+            }
+        </>
+    )
+}
+
+export default Statistics;
