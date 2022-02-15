bba338d8cbb874c9f30621cac4fd0190dda4d55b
JuanRojasC <appsjuanrojas@gmail.com>
2022-02-15 11:57:54 -0500

    Excercise unicafe step6 1.11

--- React/unicafe/src/components/Statistics.jsx
+++ React/unicafe/src/components/Statistics.jsx
@@ -9,14 +9,16 @@ const Statistics = ({good, neutral, bad}) => {
             <h2>statistics</h2>
             {
                 totalVotes?
-                    <div>
-                        <StatisticLine text='good' value={good} />
-                        <StatisticLine text='neutral' value={neutral} />
-                        <StatisticLine text='bad' value={bad} />
-                        <StatisticLine text='all' value={totalVotes} />
-                        <StatisticLine text='average' value={((good * 1) + (bad * -1)) / totalVotes} />
-                        <StatisticLine text='positive' value={((good / totalVotes) * 100) + ' %'} />
-                    </div>
+                    <table>
+                        <tbody>
+                            <StatisticLine text='good' value={good} />
+                            <StatisticLine text='neutral' value={neutral} />
+                            <StatisticLine text='bad' value={bad} />
+                            <StatisticLine text='all' value={totalVotes} />
+                            <StatisticLine text='average' value={((good * 1) + (bad * -1)) / totalVotes} />
+                            <StatisticLine text='positive' value={((good / totalVotes) * 100) + ' %'} />
+                        </tbody>
+                    </table>
                 :
                     <div>No feedback given</div>
             }
