8b6c75a40e4e9a7cc74beb5b27f964abc50c8ed4
JuanRojasC <appsjuanrojas@gmail.com>
2022-02-22 10:27:29 -0500

    Excercise course information step7 2.2

--- Comunicating with Server/course_information/src/components/Content.jsx
+++ Comunicating with Server/course_information/src/components/Content.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Part } from './Part'
 
 export const Content = ({parts}) => {
-  return (
-    <div>
-        {parts.map(part => <Part key={part.id} part={part}/>)}
-    </div>
-  )
+
+    return (
+        <div>
+            {parts.map(part => <Part key={part.id} part={part}/>)}
+            <b>total of {parts.reduce((a, b) => a + b.exercises, 0)} excercises</b>
+        </div>
+    )
 }
