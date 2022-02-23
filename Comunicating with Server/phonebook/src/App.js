561be4716beabfb47c4822d09c47844e7aa413a0
JuanRojasC <appsjuanrojas@gmail.com>
2022-02-23 10:48:20 -0500

    Excercise The phonebook step4 2.9*

--- Comunicating with Server/phonebook/src/App.js
+++ Comunicating with Server/phonebook/src/App.js
@@ -2,26 +2,30 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas', phone: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filterName, setFilterName] = useState('')
+  const [personsFiltered, setPersonsFiltered] = useState([])
 
   const handlerAddPerson = (e) => {
     e.preventDefault()
+    setPersonsFiltered([])
     if(checkNameNotAlreadyExists(newName)){
-      setPersons(persons.concat({name: newName, phone: newPhone, id: persons.length + 1}))
+      setPersons(persons.concat({name: newName, number: newPhone, id: persons.length + 1}))
     }else{
       alert(`${newName} is already added to phonebook`)
     }
   }
 
-  const handlerNewName = (e) => {
-    setNewName(e.target.value)
-  }
-
-  const handlerNewPhone = (e) => {
-    setNewPhone(e.target.value)
+  const handlerFindPerson = (e) => {
+    e.preventDefault()
+    setPersonsFiltered(persons.filter(person => person.name.toLocaleLowerCase().includes(filterName.toLocaleLowerCase())))
   }
 
   const checkNameNotAlreadyExists = (name) => {
@@ -31,19 +35,30 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handlerAddPerson}>
+      <form onSubmit={handlerFindPerson}>
         <div>
-          name: <input onChange={handlerNewName}/>
+          filter show with: <input onChange={e => setFilterName(e.target.value)}/>
         </div>
         <div>
-          number: <input onChange={handlerNewPhone}/>
+          <button type="submit">find</button>
+        </div>
+      </form>
+      <h2>Add a New</h2>
+      <form onSubmit={handlerAddPerson}>
+        <div>
+          name: <input onChange={e => setNewName(e.target.value)}/>
+        </div>
+        <div>
+          number: <input onChange={e => setNewPhone(e.target.value)}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{persons.map(person => <div key={person.id}>{person.name} {person.phone}</div>)}</div>
+      <div>{personsFiltered.length === 0? persons.map(person => <div key={person.id}>{person.name} {person.number}</div>) 
+                                      : personsFiltered.map(person => <div key={person.id}>{person.name} {person.number}</div>)}
+      </div>
     </div>
   )
 }
