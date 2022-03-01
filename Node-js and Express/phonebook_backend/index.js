3a93ce4ad19928215a939fbd0c0a7b90a71ccf06
JuanRojasC <appsjuanrojas@gmail.com>
2022-03-01 18:55:16 -0500

    Exercise Phonebook backend step8 3.8*

--- Node-js and Express/phonebook_backend/index.js
+++ Node-js and Express/phonebook_backend/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+// app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 var persons = [
     { 
@@ -85,16 +86,20 @@ app.post('/api/persons', (request, response) => {
   
 })
 
-// MORGAN
+// MORGAN (3.7)
 morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
     tokens.url(req, res),
     tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms'
+    tokens.res(req, res, 'content-length'),
+    tokens['response-time'](req, res), 'ms',
+    tokens.body(req, res)
   ].join(' ')
-})  
+})
+
+// MORGAN TOKENS (3.8)
+morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 const PORT = 3001
 app.listen(PORT, () => {
